2783e3bbb0842cb796ff259fe737249e83716fe1
Dominic <dominic.marcelino@utomic.media>
2024-04-04 16:58:29 +0200

    FIX: requireExtension compatability with directus 10.4

--- src/classes/HookUtils.ts
+++ src/classes/HookUtils.ts
@@ -1,7 +1,15 @@
-import type { HookExtensionContext } from '@directus/extensions';
+import type { HookExtensionContext, ApiOutput } from '@directus/extensions';
 import { createError } from '@directus/errors';
 import { defu } from "defu";
 
+
+export type RequireExtensionOptions = {
+  throwError?: boolean
+};
+
+export type RequireEnvOptions = {
+  throwError?: boolean
+};
 
 export class HookUtils {
 
@@ -35,7 +43,7 @@ export class HookUtils {
    * Makes sure an env required by the extension is set
    * @since 0.0.1
    */
-  public requireEnv(name: string, _options?: { throwError?: boolean }) {
+  public requireEnv(name: string, _options?: RequireEnvOptions) {
     const { logger, env } = this.getContext();
 
     const options = defu(_options, {
@@ -59,8 +67,8 @@ export class HookUtils {
   }
 
 
-  public async requireExtension(extensionBundle: string | null, extensionName: string, _options?: { throwError?: boolean }) {
-    const { services, logger, database, getSchema } = this.apiExtensionContext;
+  public async requireExtension(extensionName: string, extensionBundle: string | null, _options?: RequireExtensionOptions) {
+    const { services, database, getSchema } = this.apiExtensionContext;
     const { ExtensionsService } = services;
     const schema = await getSchema();
 
@@ -75,30 +83,62 @@ export class HookUtils {
       },
       schema: schema,
     });
-    const extension = await extensionsService.readOne(extensionBundle, extensionName);
 
-    if (!extension) {
-      const errorMessage = this.getLoggerMessage(`Required extension is missing: ${extensionBundle}/${extensionName}`)
-      logger.error(errorMessage);
 
-      if (options.throwError) {
-        const ExtensionNotFoundException = createError('EXTENSION_NOT_FOUND', errorMessage, 503);
-        throw new ExtensionNotFoundException;
+    // NOTE: findOne would be more persormant in case we have a lot of extensions, but as it doesn't exist (on v10.10.5), we use readAll instead
+    // as we won't check this often, and there shouldn't be too much extensions it should be okay
+    const extensions: ApiOutput[] = await extensionsService.readAll();
+    
+    let bundleID: string | null = null;
+
+    if (extensionBundle) {
+      const bundle = extensions.find((ext) => ext.schema?.name === extensionBundle) || null;
+
+      // Missing bundle
+      if (!bundle) {
+        const errorMessage = this.getLoggerMessage(`Required bundle is missing: ${extensionBundle}/${extensionName}`);
+        await this.logMissingextension(errorMessage, options);
+        return false;
       }
+
+      // Disabled bundle
+      if (bundle && !bundle.meta.enabled) {
+        const errorMessage = this.getLoggerMessage(`Required bundle is disabled: ${extensionBundle}/${extensionName}`);
+        await this.logMissingextension(errorMessage, options);
+        return false;
+      }
+
+      bundleID = bundle.id;
+    }
+    
+    // find the extension with the given name and the given ID 
+    // NOTE: if it's not part of a bundle, the ID MUST! be null
+    const extension: ApiOutput|null = extensions.find((ext) => ext.schema?.name === extensionName && ext.bundle === bundleID) || null;
+
+    if (!extension) { 
+      const errorMessage = this.getLoggerMessage(`Required extension is missing: ${extensionBundle}/${extensionName}`);
+      await this.logMissingextension(errorMessage, options);
       return false;
     }
 
     if (!extension.meta.enabled) {
-      const errorMessage = this.getLoggerMessage(`Required extension is disabled: ${extensionBundle}/${extensionName}`)
-      logger.error(errorMessage);
-
-      if (options.throwError) {
-        const ExtensionDisabledException = createError('EXTENSION_DISABLED', errorMessage, 503);
-        throw new ExtensionDisabledException;
-      }
+      const errorMessage = this.getLoggerMessage(`Required extension is disabled: ${extensionBundle}/${extensionName}`);
+      await this.logMissingextension(errorMessage, options);
       return false;
     }
 
     return true;
+  }
+
+
+
+  private async logMissingextension(message: string, options: RequireExtensionOptions) {
+    const { logger } = this.getContext();
+    logger.error(message);
+
+    if (options.throwError) {
+      const MissingExtensionError = createError('EXTENSION_DISABLED', message, 503);
+      throw new MissingExtensionError;
+    }
   }
 }
